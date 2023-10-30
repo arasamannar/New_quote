2b6d144729b9e6f5deb795fed456e8e055749ec6
Arasamannar A K <arasamannar@gmail.com>
2023-10-30 16:06:56 +0530

    Added files related to the project

---
+++ config.js
@@ -0,0 +1,4 @@
+// this page has been created to hide the API key from source code.
+
+const MY_API_KEY = '008959270emsh36d9e69819437edp106d4ajsn023665c1dabf';
+export { MY_API_KEY };
---
+++ script.js
@@ -0,0 +1,25 @@
+import { MY_API_KEY } from './config.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "quotes15.p.rapidapi.com",
+          "x-rapidapi-key": MY_API_KEY
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        
+        document.getElementById('quote').innerHTML = response.content;
+        document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    });
+  });
+  
